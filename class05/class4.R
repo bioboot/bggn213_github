6a5ae02442ba3b4037251f3d2482692d887b8fdc
Barry Grant <bjgrant@ucsd.edu>
2025-02-26 14:57:55 -0800

    Add class 5 to 8

---
+++ class05/class4.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "My first R Script report"
+#' author: "Barry"
+#' ---
+
+
+# This is just dumb text
+plot(1:10)
+
+x <- 1:50
+plot(x, sin(x), typ="l", col="blue", lwd=3, xlab="stop-now")
